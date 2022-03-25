12da43504f3afbe0e0916b6669cd448de0489715
Arpan Patel <ap.patel1893@gmail.com>
2022-03-25 13:21:36 -0500

    get row and col numbers

--- app.js
+++ app.js
@@ -1,27 +1,52 @@
-console.log("Sudoku")
+console.log("Sudoku");
 
 const BOARD_LEN = 81;
-const DIGITS = [1,2,3,4,5,6,7,8,9];
+const DIGITS = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 // create board
 const boardDiv = document.getElementById("board");
 for (let i = 0; i < BOARD_LEN; i++) {
   // <div class="square" id="i">i</div>
   let square = document.createElement("div");
-  square.id = "square-"+ i.toString();
+  square.id = "square-" + i.toString();
   square.classList.add("square");
   square.innerText = i;
-  boardDiv.append(square)
+  boardDiv.append(square);
 }
-
 
 // create digits
 const digitsDiv = document.getElementById("digits");
 for (let i = 1; i <= DIGITS.length; i++) {
-  // <div class="digit" id="i">i</div>
   let digit = document.createElement("div");
-  digit.id = "digit-"+ i.toString();
+  digit.id = "digit-" + i.toString();
   digit.classList.add("digit");
   digit.innerText = i;
-  digitsDiv.append(digit)
+  digitsDiv.append(digit);
 }
+
+const getRowBySquare = (index) => {
+  if (index >= 0 && index < BOARD_LEN) {
+    let rowNum = 0;
+    for (let i = 0; i < BOARD_LEN; i++) {
+      if (i > 0 && i % 9 === 0) {
+        rowNum++;
+        console.log("rowNum", rowNum);
+      }
+      if (index === 0 || index === i) {
+        return rowNum;
+      }
+    }
+  }
+  return null;
+};
+
+const getColumnBySquare = (index) => {
+  if (index >= 0 && index < BOARD_LEN) {
+    return index % 9;
+  }
+  return null;
+};
+
+const getBoxBySquare = (index) => {
+  return null;
+};
